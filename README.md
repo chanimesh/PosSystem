9f37c3a4bc2f4c9ae3e4e2e55d599cd2badf32dd
Animesh <animeshc@thoughtworks.com>
2018-05-14 20:21:06 +0530

    added requirements to the readme and added webpack config and installed relevant node modules

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # posSystem
 A simple point of sale system
+
+Requirements
+* considering a touch screen
+* A simple ui with list of items
+* on touch of each item should get added to a cart
+* There should be a discount to be added to the total 
+
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    mode: "development",
+    devtool: 'inline-source-map',
+    watch: true,
+    devServer: {
+        contentBase: './dist',
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+            test: /\.scss$/,
+            use: [
+                {
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader", options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: "sass-loader", options: {
+                        sourceMap: true
+                    }
+                },
+
+            ]
+        },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader:'file-loader'
+            },
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            {from: 'src/images/*', to: 'images/', toType: 'dir', flatten: true },
+            {from: 'index.html', to: 'index.html'}
+        ],{ debug: 'debug' })
+    ]
+}
